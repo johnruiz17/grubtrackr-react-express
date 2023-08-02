00c09378d85ea90ff512a0f038ed7e52e537938e
Darren Pavel <darren.pavel@gmail.com>
2023-08-02 15:12:40 -0700

    Map displays on load (centered on San Fran) Map markers now open and close individually

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -16,6 +16,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { moveCenter } from '../slices/googleSlice';
 const { GOOGLE_API_KEY } = require('../../server/envVars');
 
+async function setCenter() {
+  const res = await fetch('http://localhost:3000/google/');
+  return await res.json();
+}
+
 export default function Map() {
   const [activeMarker, setActiveMarker] = useState(null);
   const dispatch = useDispatch();
@@ -44,8 +49,12 @@ export default function Map() {
     setActiveMarker(id);
   }
 
-  const onLoad = useCallback((map) => {
-    return (mapRef.current = map);
+  const onLoad = useCallback(async (map) => {
+    const center = await setCenter();
+
+    dispatch(moveCenter(center));
+    mapRef.current = map;
+    mapRef.current?.panTo(center);
   }, []);
   // const restaurants = useMemo(() => filterRestaurants, [state.google.center]);
   const center = useSelector((state) => {
@@ -53,26 +62,11 @@ export default function Map() {
   });
 
   const restaurants = useSelector((state) => state.restaurants.restList);
-  console.log(restaurants);
-
-  useEffect(() => {
-    async function setCenter() {
-      const res = await fetch('http://localhost:3000/google/');
-      const center = await res.json();
-      console.log(center);
-
-      mapRef.current?.panTo(center);
-      dispatch(moveCenter(center));
-      return center;
-    }
-
-    setCenter();
-  }, []);
 
   return isLoaded ? (
     <div id='mapiframe'>
       <GoogleMap
-        zoom={defaultZoom}
+        zoom={14}
         center={defaultCenter}
         options={options}
         onLoad={onLoad}
@@ -84,15 +78,15 @@ export default function Map() {
               console.log(rest);
               return (
                 <Marker
-                  key={rest._id}
+                  key={rest.id}
                   position={{
                     lat: rest.coordinates.latitude,
                     lng: rest.coordinates.longitude,
                   }}
                   title={rest.name}
-                  onClick={() => handleActiveMarker(rest._id)}
+                  onClick={() => handleActiveMarker(rest.id)}
                 >
-                  {activeMarker === rest._id ? (
+                  {activeMarker === rest.id ? (
                     <InfoWindow onCloseClick={() => setActiveMarker(null)}>
                       <div>{rest.name}</div>
                     </InfoWindow>
