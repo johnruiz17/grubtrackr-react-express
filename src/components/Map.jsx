a7ac99abf6447900b434b3eebce804627ec55e6f
liamroh <liamrohx@gmail.com>
2023-08-02 12:35:59 -0700

    fixed api key

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import '../styles/main.scss';
+import { GOOGLE_API_KEY } from '../../server/envVars';
 
 const Mapcontainer = () => {
 	return (
 		<div>
-			<iframe id='mapiframe' loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/search?q=restaurants%20near%20San%20Francisco%2C%20CA%2C%20USA&key=AIzaSyD-QQgbPI08Nv84JqOuaaQrrxLXpkgrwmM"></iframe>
+			<iframe id='mapiframe' loading="lazy" allowfullscreen src={`https://www.google.com/maps/embed/v1/search?q=restaurants%20near%20San%20Francisco%2C%20CA%2C%20USA&key=${GOOGLE_API_KEY}`}></iframe>
 		</div>
 	);
 };
