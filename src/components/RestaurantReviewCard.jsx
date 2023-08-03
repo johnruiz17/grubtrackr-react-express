e9f2b9aeda77a1696cb81904b1a158f2aa3d73ad
liamroh <liamrohx@gmail.com>
2023-08-03 10:47:55 -0700

    new review card

---
+++ src/components/RestaurantReviewCard.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import '../styles/main.scss';
+
+const RestaurantReviewCard = () => {
+	const navigate = useNavigate();
+    const reviews = useSelector((state) => state.reviews);
+
+	const { name, image_url, rating, review_count, categories, price } = info;
+	console.log(categories);
+
+	return (
+		<div className='resCard'>
+			<h1>{name}</h1>
+			<h2>Cuisine: {categories[0].title}</h2>
+			<img id='restaurantPreview' src={image_url}></img>
+			<p>
+				<strong>Rating: </strong> {rating}
+			</p>
+			<p>
+				<strong>Number of reviews: </strong> {review_count}
+			</p>
+			<p>
+				<strong>Price: </strong> {price}
+			</p>
+            <p>
+				<strong>Reviews: </strong> {reviews}
+			</p>
+            <button onClick={navigate('/')}>Back</button>
+		</div>
+	);
+  };
+  
+  export default Navbar;
+  
